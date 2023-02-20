40d5ea1a49b1c6dc2b0298884fb2124aafe9875f
filip.belzik@email.cz <filip.belzik@email.cz>
2023-02-20 22:11:58 +0100

    Update script.js

--- oauth/script.js
+++ oauth/script.js
@@ -7,8 +7,6 @@ function handleCredentialResponse(response) {
   setCookie('Surname', responsePayload.family_name, 7);
   setCookie('Image', responsePayload.picture, 7);
   setCookie('Email', responsePayload.email, 7);
-
-  checkSession();
 }
 
 function logout() {

07154b5208c49943fd3be72ca38c722d80549181
zahadneokurkycz <78108685+zahadneokurkycz@users.noreply.github.com>
2026-03-17 14:09:14 +0100

    Fix variable name and improve guess function

--- guessr.js
+++ guessr.js
@@ -1,5 +1,7 @@
 function guess() {
-  let confirm = prompt("Write your number that I am gonna guess")
+  const number = prompt("Write your number that I am gonna guess");
+  if (number === null) return;
+
   document.getElementById("pepa").innerHTML = "Asking CHATGPT";
   setTimeout(() => {
     document.getElementById("pepa").innerHTML = "Asking Pepa Pig";
@@ -8,9 +10,9 @@ function guess() {
       setTimeout(() => {
         document.getElementById("pepa").innerHTML = "Asking random AI (actually an indian)";
         setTimeout(() => {
-          document.getElementById("pepa").innerHTML = "Your number is: "+confirm;
-        },5000)
-      },5000)
-    },5000)
-  },5000)
+          document.getElementById("pepa").innerHTML = "Your number is: " + number;
+        }, 5000);
+      }, 5000);
+    }, 5000);
+  }, 5000);
 }

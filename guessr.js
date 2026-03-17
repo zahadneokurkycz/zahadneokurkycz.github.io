f184bab82544ef12f2c9e4f110f2afc8cef39b73
zahadneokurkycz <78108685+zahadneokurkycz@users.noreply.github.com>
2026-03-17 14:41:34 +0100

    Refactor guessing game animations and prompts
    Updated animations and modified text prompts for guessing game.

--- guessr.js
+++ guessr.js
@@ -3,15 +3,31 @@ function guess() {
   if (number === null) return;
 
   document.getElementById("pepa").innerHTML = "Asking CHATGPT";
-  document.getElementById("idk").style.width = "100%";
+  //document.getElementById("idk").style.width = "100%";
+  $("#idk").animate({
+    width: "20%"
+}, 1000)
   setTimeout(() => {
     document.getElementById("pepa").innerHTML = "Asking Pepa Pig";
+    $("#idk").animate({
+    width: "40%"
+}, 1000)
     setTimeout(() => {
       document.getElementById("pepa").innerHTML = "Asking YOUR MOM";
-      setTimeout(() => {
-        document.getElementById("pepa").innerHTML = "Asking random AI (actually an indian)";
+      
+      $("#idk").animate({
+    width: "60%"
+}, 1000)
         setTimeout(() => {
-          document.getElementById("pepa").innerHTML = "Your number is: " + number;
+        document.getElementById("pepa").innerHTML = "Asking random AI (actually an indian)";
+        $("#idk").animate({
+    width: "80%"
+}, 1000)
+          setTimeout(() => {
+            $("#idk").animate({
+    width: "100%"
+}, 1000)
+            document.getElementById("pepa").innerHTML = "Your number is: " + number;
         }, 5000);
       }, 5000);
     }, 5000);

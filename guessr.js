38fb82b6bb39244db3885b7ec9b0a3f042accbb9
zahadneokurkycz <78108685+zahadneokurkycz@users.noreply.github.com>
2026-03-17 14:00:22 +0100

    Refactor guess function to use prompt and delays
    Updated the input method to use prompt and added multiple delayed messages before revealing the guessed number.

--- guessr.js
+++ guessr.js
@@ -1,4 +1,16 @@
 function guess() {
-  let confirm = ask("Write your number that I am gonna guess")
-  document.getElementById("pepa").innerHTML = "Asking CHATGPT"
+  let confirm = prompt("Write your number that I am gonna guess")
+  document.getElementById("pepa").innerHTML = "Asking CHATGPT";
+  setTimeout(() => {
+    document.getElementById("pepa").innerHTML = "Asking Pepa Pig";
+    setTimeout(() => {
+      document.getElementById("pepa").innerHTML = "Asking YOUR MOM";
+      setTimeout(() => {
+        document.getElementById("pepa").innerHTML = "Asking random AI (actually an indian)";
+        setTimeout(() => {
+          document.getElementById("pepa").innerHTML = "Your number is: "+confirm;
+        },5000)
+      },5000)
+    },5000)
+  },5000)
 }
